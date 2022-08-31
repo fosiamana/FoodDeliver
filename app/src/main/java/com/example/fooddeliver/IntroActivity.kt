dfbb4e395797e3def357caa16e9d02f94c1c1e5d
fosiamana <fosiamana2@gmail.com>
2022-08-31 11:28:54 +0300

    made changes

--- app/src/main/java/com/example/fooddeliver/IntroActivity.kt
+++ app/src/main/java/com/example/fooddeliver/IntroActivity.kt
@@ -10,12 +10,13 @@ import org.xml.sax.HandlerBase
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
-        SetContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+
         Handler().postDelayed({
-            val intent = Intent(packageContext this,MainActivity::class.java)
+            val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, delayMillis(2000))
+        }, 2000)
     }
-}
+
+    }
