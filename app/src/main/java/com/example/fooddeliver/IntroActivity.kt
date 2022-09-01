2db53b0b9946ec20f6428218e6a457762ee17c0e
fosiamana <fosiamana2@gmail.com>
2022-09-01 11:23:12 +0300

    made changes

--- app/src/main/java/com/example/fooddeliver/IntroActivity.kt
+++ app/src/main/java/com/example/fooddeliver/IntroActivity.kt
@@ -3,7 +3,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import kotlinx.coroutines.delay
 import org.xml.sax.HandlerBase
 
 
@@ -16,7 +15,7 @@ class IntroActivity : AppCompatActivity() {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 200)
     }
 
     }
